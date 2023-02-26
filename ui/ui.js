b033ceb7514ca67cb638292b34721e4ee892e0ed
Ali Amini Bagh <aliaminibagh@gmail.com>
2023-02-26 13:24:49 +0330

    final version 2.2

--- ui/ui.js
+++ ui/ui.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#file').change(function () {
+    $('#file').change(function() {
         var i = $(this).prev('label').clone();
         var file = $('#file')[0].files[0].name;
         $(this).prev('label').text(file);
