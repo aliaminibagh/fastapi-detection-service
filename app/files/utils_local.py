df3d40157d156edd16a3a67d53251bf1f2fe771d
Ali Amini Bagh <aliaminibagh@gmail.com>
2023-02-28 12:16:25 +0330

    version 3

--- app/files/utils_local.py
+++ app/files/utils_local.py
@@ -1,7 +1,10 @@
+from collections import deque
 import io
+import uuid
 
 import cv2
 import numpy as np
+import tensorflow as tf
 import torch
 from PIL import Image
 from ultralytics import YOLO
@@ -11,8 +14,9 @@ import tempfile
 
 def get_yolov5(name):
     # local best.pt
-    model = torch.hub.load('ultralytics/yolov5', 'custom',
-                           path=f'./app/files/model/{name}.pt')  # local repo
+    model = torch.hub.load(
+        "ultralytics/yolov5", "custom", path=f"./app/files/model/{name}.pt"
+    )  # local repo
     model.conf = 0.25
     return model
 
@@ -20,13 +24,13 @@ def get_yolov5(name):
 def get_yolov8(name="yolov8x"):
     # local model
     if name == "smoke":
-        model = YOLO('./app/files/model/smoke.pt')
+        model = YOLO("./app/files/model/smoke.pt")
     elif name == "yolov8n":
-        model = YOLO('./app/files/model/yolov8n.pt')
+        model = YOLO("./app/files/model/yolov8n.pt")
     elif name == "yolov8m":
-        model = YOLO('./app/files/model/yolov8m.pt')
+        model = YOLO("./app/files/model/yolov8m.pt")
     else:
-        model = YOLO('./app/files/model/yolov8x.pt')
+        model = YOLO("./app/files/model/yolov8x.pt")
     return model
 
 
@@ -48,14 +52,15 @@ def draw_bounding_box_on_image(image, boxes, scores, classes, threshold=0.7):
         if classes[i] == 1 and scores[i] > threshold:
             box = boxes[i]
             image = cv2.rectangle(
-                image, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
+                image, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2
+            )
     return image
 
 
 def get_video_from_bytes(binary_video):
     try:
         contents = binary_video.file.read()
-        with open(binary_video.filename, 'wb') as f:
+        with open(binary_video.filename, "wb") as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error uploading the file"}
@@ -63,11 +68,88 @@ def get_video_from_bytes(binary_video):
         binary_video.file.close()
     return cv2.VideoCapture(binary_video.filename), binary_video.filename
 
+
 def get_video_from_bytes_temp(binary_video):
     with tempfile.NamedTemporaryFile(suffix=".mp4") as tmp:
         tmp.write(binary_video.file.read())
         return cv2.VideoCapture(tmp.name), tmp.name
 
 
+def predict_on_live_video(binary_video, model, window_size = 15,classes_list=["non-violence", "violence"]):
+    # Initialize a Deque Object with a fixed size which will be used to implement moving/rolling average functionality.
+    predicted_labels_probabilities_deque = deque(maxlen=window_size)
+    video_reader, _ = get_video_from_bytes(binary_video)
+    # Reading the Video File using the VideoCapture Object
+    # video_reader = cv2.VideoCapture(binary_video)
+    ID  = uuid.uuid4()
+    # Getting the width and height of the video
+    original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+    original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*"H264")
+    fps = video_reader.get(cv2.CAP_PROP_FPS)
+    # Writing the Overlayed Video Files Using the VideoWriter Object
+    video_writer = cv2.VideoWriter(f"./ui/results/{ID}.mp4", fourcc, fps, (original_video_width, original_video_height))
+    print("Number of Frames in the Video: ", video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    while True:
 
+        # Reading The Frame
+        status, frame = video_reader.read()
+        frame_id = video_reader.get(1)
+        if not status:
+            break
 
+        # Resize the Frame to fixed Dimensions
+
+        if frame_id % 100 == 0:
+            print(f"Frame {ID} of {video_reader.get(cv2.CAP_PROP_FRAME_COUNT)}")
+
+        rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        resized_frame = cv2.resize(rgb_img, (224, 224))
+        normalized_frame = resized_frame / 255
+        # Passing the Image Normalized Frame to the model and receiving Predicted Probabilities.
+        predicted_labels_probabilities = model.predict(np.expand_dims(normalized_frame, axis=0))[0]
+
+        # Appending predicted label probabilities to the deque object
+        predicted_labels_probabilities_deque.append(predicted_labels_probabilities)
+
+        # Assuring that the Deque is completely filled before starting the averaging process
+        if len(predicted_labels_probabilities_deque) == window_size:
+
+            # Converting Predicted Labels Probabilities Deque into Numpy array
+            predicted_labels_probabilities_np = np.array(
+                predicted_labels_probabilities_deque
+            )
+
+            # Calculating Average of Predicted Labels Probabilities Column Wise
+            predicted_labels_probabilities_averaged = (
+                predicted_labels_probabilities_np.mean(axis=0)
+            )
+
+            # Converting the predicted probabilities into labels by returning the index of the maximum value.
+            predicted_label = (
+                0 if predicted_labels_probabilities_averaged[0] < 0.5 else 1
+            )
+
+            # Accessing The Class Name using predicted label.
+            predicted_class_name = classes_list[predicted_label]
+
+            # Overlaying Class Name Text Ontop of the Frame
+            cv2.putText(
+                frame,
+                predicted_class_name,
+                (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 0, 255),
+                2,
+            )
+
+        # Writing The Frame
+        video_writer.write(frame)
+
+    # Closing the VideoCapture and VideoWriter objects and releasing all resources held by them.
+    video_reader.release()
+    video_writer.release()
+    return f'/results/{ID}.mp4'
--- app/routers/fight.py
+++ app/routers/fight.py
@@ -1,17 +1,21 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from ..files.utils_local import predict_on_live_video
 
-# from ..files.segmentation import get_yolov5
-
-# model = get_yolov5()
+tf.compat.v1.disable_eager_execution()
+model = load_model("./app/files/model/fight.h5")
 
 router = APIRouter(
     prefix="/fight",
     tags=["fight"],
 )
 
-@router.get("/test")
-async def test():
-    return {"result": "ok"}
+@router.post("/infer-video", summary='Detect fight in video and return json', response_description="Something here")
+async def fight_detection_infer_video(file: UploadFile = File(...)):
+    video_path = predict_on_live_video(binary_video=file, model=model)
+    return {"video": video_path}
+
 
 
